51e0fa9c13a4f765841007c123bd46ac643bc6e2
Hendro Steven <hendro.steven@gmail.com>
2021-07-27 14:07:40 +0700

    add product entity

--- src/main/java/com/brainmatics/data/entity/Product.java
+++ src/main/java/com/brainmatics/data/entity/Product.java
@@ -1,22 +1,50 @@
 package com.brainmatics.data.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="product_code", length = 5, nullable = false, unique = true)
     private String code;
+
+    @Column(name = "product_name", length = 200, nullable = false)
     private String name;
+
     private double price;
+
+    @Column(length = 400, nullable = true)
     private String description;
 
     public Product(){
 
     }
     
-    public Product(String code, String name, double price, String description) {
+    public Product(Long id, String code, String name, double price, String description) {
+        this.id = id;
         this.code = code;
         this.name = name;
         this.price = price;
         this.description = description;
     }
     
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getCode() {
         return code;
     }
